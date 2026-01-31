6e942418a349f0bdcc6a6c2d423a5a4660849df9
Nikhil Mulay <nikhilmulay04@gmail.com>
2026-01-31 11:59:36 +0530

    created header and navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,61 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+import { Button } from "./ui/button";
+import { PenBox, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className=" fixed top">
-      <SignedOut>
-        <SignInButton>
-          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer mr-2">
-            Sign In
-          </button>
-        </SignInButton>
-        <SignUpButton>
-          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="Welth Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+        </Link>
+
+
+      {/* Action Buttons */}
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link
+              href="/dashboard"
+              className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+            >
+              <Button variant="outline">
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+            <a href="/transaction/create">
+              <Button className="flex items-center gap-2">
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </a>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
