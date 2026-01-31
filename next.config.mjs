7d9cb80840bd6f9630c1f185d8173386ed2390b4
Nikhil Mulay <nikhilmulay04@gmail.com>
2026-01-31 20:01:40 +0530

    Created Landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
