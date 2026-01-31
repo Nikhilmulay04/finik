84b82e70a8fa812aa280db9e86519f15c3f39946
Nikhil Mulay <nikhilmulay04@gmail.com>
2026-01-31 10:15:59 +0530

    Updated Authentication

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@cl
 
 const Header = () => {
   return (
-    <div>
+    <div className=" fixed top">
       <SignedOut>
         <SignInButton>
           <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer mr-2">
